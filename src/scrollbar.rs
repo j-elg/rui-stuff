1fe776d6fa80ed4277033dcc9077a322b3e4c776
Johannes Elg <johanneselg@yahoo.com>
2022-03-15 14:47:42 +0100

    Cleanup

--- src/scrollbar.rs
+++ src/scrollbar.rs
@@ -29,48 +29,32 @@ where
         state(0.0, move |width| {
             let w = width.get();
             let x = scroller.get().value * w;
+            let scope = scroller.get().width / scroller.get().content_width;
             let scroller = scroller.clone();
-            let scroller2 = scroller.clone();
 
-            println!(
-                "scoller value: {}\tw: {}\twidth: {}\tcontent w: {}",
-                scroller.get().value,
-                w,
-                scroller2.get().width,
-                scroller2.get().content_width
-            );
-
-            zstack((
-                rectangle().color(CLEAR_COLOR).drag(move |off, _state| {
-                    scroller.with_mut(|v| (*v).value = ((*v).value + off.x / w).clamp(0.0, 1.0));
-                }),
-                canvas(move |sz, vger| {
-                    let c = sz.center();
-                    let paint = vger.color_paint(BUTTON_BACKGROUND_COLOR);
-                    vger.fill_rect(
-                        euclid::rect(0.0, c.y - SLIDER_WIDTH / 2.0, sz.width(), SLIDER_WIDTH),
-                        0.0,
-                        paint,
-                    );
-                    let paint = vger.color_paint(AZURE_HIGHLIGHT);
-                    vger.fill_rect(
-                        euclid::rect(
-                            x,
-                            c.y - SLIDER_WIDTH / 2.0,
-                            w * scroller2.get().width / scroller2.get().content_width,
-                            SLIDER_WIDTH,
-                        ),
-                        2.0,
-                        paint,
-                    );
-
-                    //vger.fill_circle([x, c.y], 10.0, paint);
-                }),
-            ))
+            canvas(move |sz, vger| {
+                let c = sz.center();
+                let paint = vger.color_paint(BUTTON_BACKGROUND_COLOR);
+                vger.fill_rect(
+                    euclid::rect(0.0, c.y - SLIDER_WIDTH / 2.0, sz.width(), SLIDER_WIDTH),
+                    0.0,
+                    paint,
+                );
+                let paint = vger.color_paint(AZURE_HIGHLIGHT);
+                vger.fill_rect(
+                    euclid::rect(x, c.y - SLIDER_WIDTH / 2.0, w * scope, SLIDER_WIDTH),
+                    2.0,
+                    paint,
+                );
+            })
             .geom(move |sz| {
                 if sz.width != w {
                     width.set(sz.width)
                 }
+            })
+            .drag(move |off, _state| {
+                scroller
+                    .with_mut(|v| (*v).value = ((*v).value + off.x / w).clamp(0.0, 1.0 - scope));
             })
         })
     }
